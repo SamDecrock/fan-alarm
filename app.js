f30e14fb962eb7e57ce51dab019bed5852d701ba
Sam Decrock <sam.decrock@gmail.com>
2019-01-19 06:31:56 +0100

    Add multiple alarms

--- app.js
+++ app.js
@@ -20,9 +20,45 @@ lirc.on('receive', function (remote, button, repeat) {
 function setAlarm() {
 	if(isAlarmSet) return console.log('Alarm already set');
 
-	console.log('Starting cron job. Will wake you up at 2:30 AM');
+	console.log('Starting cron job. Will wake you up at 2:30');
 
 	var task = cron.schedule('30 2 * * *', () =>  {
+		console.log(new Date());
+
+		lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
+		    if (error) console.log(error);
+		}).then(res => {
+			console.log("ONOFF sent");
+		});
+	});
+
+	console.log('Starting cron job. Will wake you up at 6:15');
+
+	var task = cron.schedule('15 6 * * *', () =>  {
+		console.log(new Date());
+
+		lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
+		    if (error) console.log(error);
+		}).then(res => {
+			console.log("ONOFF sent");
+		});
+	});
+
+	console.log('Starting cron job. Will wake you up at 12:20');
+
+	var task = cron.schedule('20 12 * * *', () =>  {
+		console.log(new Date());
+
+		lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
+		    if (error) console.log(error);
+		}).then(res => {
+			console.log("ONOFF sent");
+		});
+	});
+
+	console.log('Starting cron job. Will wake you up at 17:15');
+
+	var task = cron.schedule('15 17 * * *', () =>  {
 		console.log(new Date());
 
 		lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
