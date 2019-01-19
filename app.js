6f5357aa9b107dd5426df1997e0dacf62069dd8d
Sam Decrock <sam.decrock@gmail.com>
2019-01-19 04:19:19 +0100

    Code cleanup + readme fixes

--- app.js
+++ app.js
@@ -4,25 +4,21 @@ const lirc = require('lirc-client')({
 const cron = require('node-cron');
 
 lirc.on('connect', () => {
-	console.log('Connected to LIRC');
-	// lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
-	//     if (error) console.log(error);
-	// }).then(res => {
-	// 	console.log("ONOFF sent");
-	// });
+	console.log('Connected to LIRC.');
+
+	console.log('Starting cron job. Will wake you up at 2:30 AM');
+
+	var task = cron.schedule('30 2 * * *', () =>  {
+		console.log(new Date());
+
+		lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
+		    if (error) console.log(error);
+		}).then(res => {
+			console.log("ONOFF sent");
+		});
+	});
 });
 
 lirc.on('receive', function (remote, button, repeat) {
     console.log('button ' + button + ' on remote ' + remote + ' was pressed!');
-});
-
-console.log('Starting cron job. Will wake you up at 2:30 AM');
-var task = cron.schedule('30 2 * * *', () =>  {
-	console.log(new Date());
-
-	lirc.sendOnce('quigg', 'ONOFF', 10).catch(error => {
-	    if (error) console.log(error);
-	}).then(res => {
-		console.log("ONOFF sent");
-	});
 });
